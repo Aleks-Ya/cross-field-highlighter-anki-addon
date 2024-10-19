4e01c01c539e13d403fc2a9d9c5d53df6fc85f20
Aleksey Yablokov <ya_al@bk.ru>
2024-10-19 09:25:51 +0700

    Test HighlightOpParams

--- cross_field_highlighter/ui/operation/highlight_op_params.py
+++ cross_field_highlighter/ui/operation/highlight_op_params.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from anki.models import NotetypeId
 from anki.notes import NoteId
 from aqt import QWidget
@@ -7,12 +9,37 @@ from cross_field_highlighter.highlighter.types import Word, FieldName, FieldName
 
 
 class HighlightOpParams:
-    def __init__(self, note_type_id: NotetypeId, note_ids: set[NoteId], parent: QWidget, source_filed: FieldName,
+    def __init__(self, note_type_id: NotetypeId, note_ids: set[NoteId], parent: Optional[QWidget],
+                 source_field: FieldName,
                  destination_fields: FieldNames, stop_words: set[Word], highlight_format: HighlightFormat):
         self.note_type_id: NotetypeId = note_type_id
         self.note_ids: set[NoteId] = note_ids
-        self.parent: QWidget = parent
-        self.source_field: FieldName = source_filed
+        self.parent: Optional[QWidget] = parent
+        self.source_field: FieldName = source_field
         self.destination_fields: FieldNames = destination_fields
         self.stop_words: set[Word] = stop_words
         self.highlight_format: HighlightFormat = highlight_format
+
+    def __str__(self):
+        fields: str = ", ".join([str(field) for field in self.destination_fields])
+        return (f"HighlightOpParams(note_type_id={self.note_type_id}, note_ids={sorted(self.note_ids)}, "
+                f"source_field={self.source_field}, destination_fields={fields}, stop_words={sorted(self.stop_words)}, "
+                f"highlight_format={self.highlight_format})")
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if not isinstance(other, HighlightOpParams):
+            return False
+        return (self.note_type_id == other.note_type_id and
+                self.note_ids == other.note_ids and
+                self.parent == other.parent and
+                self.source_field == other.source_field and
+                self.destination_fields == other.destination_fields and
+                self.stop_words == other.stop_words and
+                self.highlight_format == other.highlight_format)
+
+    def __hash__(self):
+        return hash((self.note_type_id, tuple(self.note_ids), self.source_field,
+                     tuple(self.destination_fields), tuple(self.stop_words), self.highlight_format))
---
+++ tests/ui/operation/highlight_op_params_test.py
@@ -0,0 +1,49 @@
+import pytest
+from PyQt6.QtWidgets import QWidget
+from anki.models import NoteType, NotetypeId
+from anki.notes import NoteId
+from pytestqt.qtbot import QtBot
+
+from cross_field_highlighter.highlighter.formatter.highlight_format import HighlightFormat
+from cross_field_highlighter.highlighter.types import FieldNames, Word
+from cross_field_highlighter.ui.operation.highlight_op_params import HighlightOpParams
+from tests.data import DefaultFields, Data
+
+
+@pytest.fixture
+def note_ids(td: Data) -> set[NoteId]:
+    return {td.create_basic_note_1().id, td.create_basic_note_2().id}
+
+
+@pytest.fixture
+def parent(qtbot: QtBot) -> QWidget:
+    parent: QWidget = QWidget()
+    qtbot.addWidget(parent)
+    return parent
+
+
+@pytest.fixture
+def params(basic_note_type: NoteType, note_ids: set[NoteId], bold_format: HighlightFormat,
+           parent: QWidget) -> HighlightOpParams:
+    stop_words: set[Word] = {Word("a"), Word("an")}
+    return HighlightOpParams(basic_note_type["id"], note_ids, parent, DefaultFields.basic_front_field,
+                             FieldNames([DefaultFields.basic_back_field]), stop_words,
+                             bold_format)
+
+
+def test_str(params: HighlightOpParams, note_ids: set[NoteId], basic_note_type_id: NotetypeId):
+    exp: str = (f"HighlightOpParams(note_type_id={basic_note_type_id}, "
+                f"note_ids={sorted(note_ids)}, source_field=Front, destination_fields=Back, "
+                "stop_words=['a', 'an'], highlight_format=HighlightFormat(Bold, BOLD))")
+    assert str(params) == exp
+    assert str([params]) == f"[{exp}]"
+
+
+def test_eq(params: HighlightOpParams, basic_note_type: NoteType, parent: QWidget, note_ids: set[NoteId],
+            bold_format: HighlightFormat):
+    stop_words: set[Word] = {Word("a"), Word("an")}
+    params2: HighlightOpParams = HighlightOpParams(basic_note_type["id"], note_ids, parent,
+                                                   DefaultFields.basic_front_field,
+                                                   FieldNames([DefaultFields.basic_back_field]), stop_words,
+                                                   bold_format)
+    assert params == params2
