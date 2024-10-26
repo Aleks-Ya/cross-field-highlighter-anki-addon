29a6cacf79802ccb577df891b06d5df61ccc47fe
Aleksey Yablokov <ya_al@bk.ru>
2024-10-26 08:17:04 +0700

    Add NoteTypeDetailsFactory#by_note_type_name()

--- cross_field_highlighter/highlighter/note_type_details_factory.py
+++ cross_field_highlighter/highlighter/note_type_details_factory.py
@@ -7,6 +7,7 @@ from anki.models import NotetypeId, NoteType
 from anki.notes import NoteId
 
 from cross_field_highlighter.highlighter.note_type_details import NoteTypeDetails
+from cross_field_highlighter.highlighter.types import NoteTypeName
 
 log: Logger = logging.getLogger(__name__)
 
@@ -14,15 +15,23 @@ log: Logger = logging.getLogger(__name__)
 class NoteTypeDetailsFactory:
     def __init__(self, col: Collection):
         self.__col: Collection = col
-        self.__cache: dict[NotetypeId, NoteTypeDetails] = {}
+        self.__cache_id: dict[NotetypeId, NoteTypeDetails] = {}
+        self.__cache_name: dict[NoteTypeName, NoteTypeDetails] = {}
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def by_note_type_id(self, note_type_id: NotetypeId) -> NoteTypeDetails:
-        if note_type_id not in self.__cache:
+        if note_type_id not in self.__cache_id:
             note_type: NoteType = self.__col.models.get(note_type_id)
-            self.__cache[note_type_id] = NoteTypeDetails(note_type_id, note_type["name"],
-                                                         self.__col.models.field_names(note_type))
-        return self.__cache[note_type_id]
+            self.__cache_id[note_type_id] = NoteTypeDetails(note_type_id, note_type["name"],
+                                                            self.__col.models.field_names(note_type))
+        return self.__cache_id[note_type_id]
+
+    def by_note_type_name(self, note_type_name: NoteTypeName) -> NoteTypeDetails:
+        if note_type_name not in self.__cache_name:
+            note_type: NoteType = self.__col.models.by_name(note_type_name)
+            self.__cache_name[note_type_name] = NoteTypeDetails(note_type["id"], note_type_name,
+                                                                self.__col.models.field_names(note_type))
+        return self.__cache_name[note_type_name]
 
     def by_note_ids(self, note_ids: Sequence[NoteId]) -> list[NoteTypeDetails]:
         note_type_ids: set[NotetypeId] = {self.__col.get_note(note_id).mid for note_id in note_ids}
--- tests/highlighter/note_type_details_factory_test.py
+++ tests/highlighter/note_type_details_factory_test.py
@@ -3,12 +3,18 @@ from anki.notes import Note, NoteId
 
 from cross_field_highlighter.highlighter.note_type_details import NoteTypeDetails
 from cross_field_highlighter.highlighter.note_type_details_factory import NoteTypeDetailsFactory
+from cross_field_highlighter.highlighter.types import NoteTypeName
 from tests.data import Data
 
 
 def test_by_note_type_id(note_type_details_factory: NoteTypeDetailsFactory, basic_note_type_id: NotetypeId,
                          basic_note_type_details: NoteTypeDetails):
     assert note_type_details_factory.by_note_type_id(basic_note_type_id) == basic_note_type_details
+
+
+def test_by_note_type_name(note_type_details_factory: NoteTypeDetailsFactory, basic_note_type_name: NoteTypeName,
+                           basic_note_type_details: NoteTypeDetails):
+    assert note_type_details_factory.by_note_type_name(basic_note_type_name) == basic_note_type_details
 
 
 def test_by_note_ids(note_type_details_factory: NoteTypeDetailsFactory, td: Data,
