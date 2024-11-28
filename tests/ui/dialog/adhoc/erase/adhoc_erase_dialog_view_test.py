acbd90bed232e81a8b7928b2c59a6f8c73255987
Aleksey Yablokov <ya_al@bk.ru>
2024-11-28 10:40:16 +0700

    Add test_remember_selected_fields_when_changing_note_type

--- tests/ui/dialog/adhoc/erase/adhoc_erase_dialog_view_test.py
+++ tests/ui/dialog/adhoc/erase/adhoc_erase_dialog_view_test.py
@@ -104,5 +104,87 @@ def test_show_view(adhoc_erase_dialog_view: AdhocEraseDialogView, adhoc_erase_di
                                                                        'selected_note_type': cloze_note_type_details}}}
 
 
+def test_remember_selected_fields_when_changing_note_type(
+        adhoc_erase_dialog_view: AdhocEraseDialogView, adhoc_erase_dialog_model: AdhocEraseDialogModel,
+        basic_note_type_details: NoteTypeDetails, cloze_note_type_details: NoteTypeDetails,
+        all_note_type_details: list[NoteTypeDetails], adhoc_erase_dialog_view_scaffold: AdhocEraseDialogViewScaffold,
+        visual_qtbot: VisualQtBot):
+    callback: FakeCallback = FakeCallback()
+    listener: FakeModelListener = FakeModelListener()
+    adhoc_erase_dialog_model.add_listener(listener)
+    # noinspection PyUnresolvedReferences
+    adhoc_erase_dialog_view.show()
+    visual_qtbot.waitExposed(adhoc_erase_dialog_view)
+    # Initial state
+    adhoc_erase_dialog_model.fill(all_note_type_details, callback.call, None)
+    adhoc_erase_dialog_view.show_view()
+    assert_view(adhoc_erase_dialog_view, check_box_texts=DefaultFields.all_basic, selected_fields=[])
+    assert len(listener.history) == 2
+    assert adhoc_erase_dialog_model.as_dict() == {'note_types': all_note_type_details,
+                                                  'accept_callback_None': False,
+                                                  'reject_callback_None': True,
+                                                  'current_state': {'selected_fields': [],
+                                                                    'selected_note_type': basic_note_type_details},
+                                                  'states': {'Basic': {'selected_fields': [],
+                                                                       'selected_note_type': basic_note_type_details}}}
+
+    # Choose field for basic
+    adhoc_erase_dialog_view_scaffold.mark_destination_field()
+    assert_view(adhoc_erase_dialog_view, check_box_texts=DefaultFields.all_basic,
+                selected_fields=[DefaultFields.basic_front])
+    assert len(listener.history) == 3
+    assert adhoc_erase_dialog_model.as_dict() == {'note_types': all_note_type_details,
+                                                  'accept_callback_None': False,
+                                                  'reject_callback_None': True,
+                                                  'current_state': {'selected_fields': [DefaultFields.basic_front],
+                                                                    'selected_note_type': basic_note_type_details},
+                                                  'states': {'Basic': {'selected_fields': [DefaultFields.basic_front],
+                                                                       'selected_note_type': basic_note_type_details}}}
+
+    # Choose Note Type: cloze
+    adhoc_erase_dialog_view_scaffold.select_note_type(Qt.Key.Key_Down)
+    assert_view(adhoc_erase_dialog_view, check_box_texts=DefaultFields.all_cloze, selected_fields=[])
+    assert len(listener.history) == 4
+    assert adhoc_erase_dialog_model.as_dict() == {'note_types': all_note_type_details,
+                                                  'accept_callback_None': False,
+                                                  'reject_callback_None': True,
+                                                  'current_state': {'selected_fields': [],
+                                                                    'selected_note_type': cloze_note_type_details},
+                                                  'states': {'Basic': {'selected_fields': [DefaultFields.basic_front],
+                                                                       'selected_note_type': basic_note_type_details},
+                                                             'Cloze': {'selected_fields': [],
+                                                                       'selected_note_type': cloze_note_type_details}}}
+
+    # Choose field for cloze
+    adhoc_erase_dialog_view_scaffold.mark_destination_field()
+    assert_view(adhoc_erase_dialog_view, check_box_texts=DefaultFields.all_cloze,
+                selected_fields=[DefaultFields.cloze_text])
+    assert len(listener.history) == 5
+    assert adhoc_erase_dialog_model.as_dict() == {'note_types': all_note_type_details,
+                                                  'accept_callback_None': False,
+                                                  'reject_callback_None': True,
+                                                  'current_state': {'selected_fields': [DefaultFields.cloze_text],
+                                                                    'selected_note_type': cloze_note_type_details},
+                                                  'states': {'Basic': {'selected_fields': [DefaultFields.basic_front],
+                                                                       'selected_note_type': basic_note_type_details},
+                                                             'Cloze': {'selected_fields': [DefaultFields.cloze_text],
+                                                                       'selected_note_type': cloze_note_type_details}}}
+
+    # Choose Note Type: basic
+    adhoc_erase_dialog_view_scaffold.select_note_type(Qt.Key.Key_Up)
+    assert_view(adhoc_erase_dialog_view, check_box_texts=DefaultFields.all_basic,
+                selected_fields=[DefaultFields.basic_front])
+    assert len(listener.history) == 6
+    assert adhoc_erase_dialog_model.as_dict() == {'note_types': all_note_type_details,
+                                                  'accept_callback_None': False,
+                                                  'reject_callback_None': True,
+                                                  'current_state': {'selected_fields': [DefaultFields.basic_front],
+                                                                    'selected_note_type': basic_note_type_details},
+                                                  'states': {'Basic': {'selected_fields': [DefaultFields.basic_front],
+                                                                       'selected_note_type': basic_note_type_details},
+                                                             'Cloze': {'selected_fields': [DefaultFields.cloze_text],
+                                                                       'selected_note_type': cloze_note_type_details}}}
+
+
 def test_repr(adhoc_erase_dialog_view: AdhocEraseDialogView):
     assert repr(adhoc_erase_dialog_view) == "AdhocEraseDialogView"
