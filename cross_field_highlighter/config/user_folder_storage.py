353e8ac6f05e750ad8e2fac60537a2edf7fc6c9a
Aleksey Yablokov <ya_al@bk.ru>
2025-01-18 10:59:24 +0700

    Refactor UserFolderStorage

--- cross_field_highlighter/config/user_folder_storage.py
+++ cross_field_highlighter/config/user_folder_storage.py
@@ -1,7 +1,7 @@
+import json
 import logging
 from logging import Logger
 from pathlib import Path
-import json
 
 from .settings import Settings
 
@@ -10,27 +10,32 @@ log: Logger = logging.getLogger(__name__)
 
 class UserFolderStorage:
     def __init__(self, settings: Settings):
-        self.__storage_file: Path = settings.user_folder / "storage.json"
+        self.__settings: Settings = settings
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def read(self, key: str) -> dict[str, any]:
         return self.read_all().get(key, {})
 
     def write(self, key: str, value: dict[str, any]) -> None:
-        if not self.__storage_file.parent.exists():
-            self.__storage_file.parent.mkdir(parents=True)
+        storage_file: Path = self.__get_storage_file()
+        if not storage_file.parent.exists():
+            storage_file.parent.mkdir(parents=True)
         current_data: dict[str, any] = self.read_all()
         current_data[key] = value
-        with open(self.__storage_file, 'w') as f:
+        with open(storage_file, 'w') as f:
             # noinspection PyTypeChecker
             json.dump(current_data, f, indent=2)
 
     def read_all(self) -> dict[str, any]:
-        if self.__storage_file.exists():
-            with open(self.__storage_file, 'r') as f:
+        storage_file: Path = self.__get_storage_file()
+        if storage_file.exists():
+            with open(storage_file, 'r') as f:
                 return json.load(f)
         else:
             return {}
 
+    def __get_storage_file(self) -> Path:
+        return self.__settings.user_folder / "storage.json"
+
     def __str__(self):
-        return f"UserFolderStorage({self.__storage_file})"
+        return f"UserFolderStorage({self.__get_storage_file()})"
